7510b6882f77ad244b931301410b946896d42ba2
SimonHank <149795586+SimonHank@users.noreply.github.com>
2023-11-03 15:11:39 +0100

    neue Version Test

--- exercise-001/main.cpp
+++ exercise-001/main.cpp
@@ -5,17 +5,30 @@
 #include <random>
 
 #include "CLI/CLI.hpp"
-#include "config.h"
+//#include "config.h"
 
+//.bss segment
+int foo;
+
+//.data segment
+int data = 4711;
+
+//.rodata segment
+const int rodata = 4711;
 
 auto main(int argc, char **argv) -> int
-{
-
-    /**
+{    /**
      * The {fmt} lib is a cross platform library for printing and formatting text
      * it is much more convenient than std::cout and printf
      * More info at https://fmt.dev/latest/api.html
      */
-    fmt::print("Hello World!\n");
-    return 0; /* exit gracefully*/
+        fmt::print("Value of variable bss {} address of variable bss {}",bss,fmt::ptr(&bss));
+
+        fmt::print("Value of variable data {} address of variable data {}",data,fmt::ptr(&data));
+
+        fmt::print("Value of variable rodata {} address of variable rodata {}",rodata,fmt::ptr(&rodata));
+    return 0; /* exit gracefully*/
+}
+
+
 }
